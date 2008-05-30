2129bb9833b2d2b729f0c189659c4f81d7fa0cba
starpeak <starpeak@Caradhras>
2008-05-30 17:37:24 +0200

    Added progress with themes and functional test

---
+++ src/progress/progress.js
@@ -0,0 +1,46 @@
+UI.Progress = Class.create(UI.Options, {
+  options: {
+    width: 400,
+    theme: 'progress',
+    maxValue: 100,
+    onUpdate: false
+  },
+  initialize: function(element, options){
+    this.element = $(element);
+    this.setOptions(options);
+    this.pane = new Element('div', {
+      className: 'pane',
+      style: 'width:' + this.options.width + 'px'
+    });
+    this.bar = new Element('div', {
+      className: 'bar working',
+      style: 'width:' + this.options.width + 'px'
+    });
+    this.pane.insert(this.bar);
+    this.element.insert(new Element('div', {
+      className: 'UI-progress ' + this.options.theme
+    }).insert(this.pane));
+    this.value = '';
+  },
+  setValue: function(v){
+    if (v < 0) {
+      this.bar.style.width = this.options.width + 'px';
+      this.bar.addClassName('working');
+      this.value = '';
+    }
+    else {
+      if (this.bar.hasClassName('working')) 
+        this.bar.removeClassName('working');
+      if (v > this.options.maxValue) 
+        v = this.options.maxValue;
+      var width = parseInt(this.options.width * v / this.options.maxValue);
+      this.bar.style.width = width + 'px';
+      this.value = v;
+    }
+    
+    if (this.options.onUpdate) 
+      this.options.onUpdate(this);
+    
+    return this;
+  }
+});
