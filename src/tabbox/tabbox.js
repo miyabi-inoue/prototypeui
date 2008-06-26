b67d7841c13e530d44e057cdfdaf9a2c70dcc043
Sven G. Brönstrup <broenstrup@spom.net>
2008-06-26 16:24:06 +0200

    First tabbox version with 2 themes

--- src/tabbox/tabbox.js
+++ src/tabbox/tabbox.js
@@ -1,6 +1,8 @@
 UI.Tabbox = Class.create(UI.Options, {
   // Group: Options
-  options: {},
+  options: {
+
+  },
  
   // Group: Contructor
   
@@ -10,13 +12,57 @@ UI.Tabbox = Class.create(UI.Options, {
       The tabbox is not open and nothing has been added to the DOM yet
       
     Parameters:                                                                            
+      element - Element to make a tabbox
+      tabs    - (Array) tab definition
       options - (Hash) list of optional parameters  
       
     Returns:
       this
   */
-  initialize: function(options) {
+  initialize: function(element, tabs, options) {
     this.setOptions(options);
+
+    this.element               = $(element);  
+    this.tabs                  = new Hash;
+
+    this.element.childElements().invoke('hide');
+    this.element.addClassName(this.options.theme);
+    
+    this.head=new Element('div',{className:'tabbox_head'});
+    
+    tabs.each(function(tab) {
+      
+      head=new Element('span').insert(new Element('span',{className:'tab_w'})).insert(new Element('span',{className:'tab_c'}).insert(tab.lable)).insert(new Element('span',{className:'tab_e'}));
+      head.writeAttribute('pui-tabbox:name',tab.name)
+      head.observe('click',function(e){
+        this.select(tab.name);
+      }.bind(this).bind(tab));
+      tab.head=head;
+      this.head.insert(head);
+      
+      if(tab.element)
+        tab.element=$(tab.element);
+      else {
+        tab.element=new Element('div').hide();
+        this.element.insert({
+          bottom: tab.element
+        });
+        if(tab.content) {
+          tab.element.insert(tab.content);
+        }
+      }
+      tab.element.addClassName('tabbox_body');
+      tab.element.writeAttribute('pui-tabbox:object',this);
+          
+      this.tabs.set(tab.name,tab);
+    }.bind(this));
+    
+    this.element.insert({
+        top: this.head
+      });
+      
+    return this;
+    //this.change(this.openTab);
   },
 
   /*
@@ -80,4 +126,72 @@ UI.Tabbox = Class.create(UI.Options, {
 
   // Group: Actions
 
+  select: function(tab) {
+    if (this.selectedTab && this.selectedTab.name == tab) {
+      return this;
+    }
+    
+    if(this.selectedTab) {
+      this.selectedTab.head.removeClassName('selected');
+      this.selectedTab.element.hide();
+    }
+    
+    this.selectedTab=this.tabs.get(tab);
+    
+    if(this.selectedTab.ajaxContent)
+    {
+      tab=this.tabs.get(tab);
+      options={onComplete:function(){
+        this.fire('selected', {
+          tab: tab
+        });
+        if(tab.ajaxLoadOnce) {
+          tab.ajaxContent=false;
+        }
+      }.bind(this).bind(tab)};
+      new Ajax.Updater(this.selectedTab.element, this.selectedTab.ajaxContent, options);
+    } else {
+      this.fire('selected', {
+        tab: tab
+      });
+    }
+    
+    this.selectedTab.head.addClassName('selected');
+    this.selectedTab.element.show();
+      
+    
+    return this;
+  },
+
+  change: function(tab) {
+    tab_div=this.name+'_tab_'+tab;
+    head_div=this.name+'_head_'+tab;
+    
+    if(this.options.ajaxTabs && this.options.ajaxTabs[tab]) {
+      target=$(this.name+'_tab_'+tab);
+      if(target.firstChild)
+        target=$(target.firstChild);
+        
+      ajaxtab=this.options.ajaxTabs[tab];
+      this.options.ajaxTabs[tab]=false;
+        
+      ajaxtab.options.onSuccess=this.change(tab);
+        
+      new Ajax.Updater(target, ajaxtab.url, ajaxtab.options);
+      
+      return false;
+    }
+        
+    if(this.openTab!=tab) {
+      $(this.name+'_head_'+this.openTab).removeClassName("selected");
+        $(head_div).addClassName("selected");
+    
+      new Effect.Fade(this.name+'_tab_'+this.openTab, { duration: 0.15, afterFinish: function(){new Effect.Appear(tab_div, { duration: 0.15, afterFinish: function(){} });} });
+      this.openTab=tab;
+    }
+    
+    this.fire('',{tab:tab});
+
+    return this;
+  }
 });
